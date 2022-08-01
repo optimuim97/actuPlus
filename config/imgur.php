3c2be8c23496197123c88f82f6ebb4f0e8dd014b
seri jean setone <jean.seri@uniskip.com>
2022-08-01 12:43:41 +0000

    'r'

--- config/imgur.php
+++ config/imgur.php
@@ -5,9 +5,9 @@ return [
      * Set Imgur application client_id and secret key, endpoint.
      */
 
-    'client_id' => env('IMGUR_CLIENT_ID'),
+    'client_id' => '0bc6fefc4d8f6fb',
 
-    'client_secret' => env('IMGUR_CLIENT_SECRET', null),
+    'client_secret' => env('IMGUR_CLIENT_SECRET', '448c92f48f6e65738a1f0121a7c689d11bdf239c'),
 
     'endpoint' => env('IMGUR_ENDPOINT', 'https://api.imgur.com/3/image'),
 ];
